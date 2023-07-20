306eab94cda9139d2bcdf241e4fbd701ca9bfac7
svobodamichal <svoboda.michalg@gmail.com>
2023-07-20 14:09:28 +0200

    Hard path correction 5

--- container.sh
+++ container.sh
@@ -4,4 +4,5 @@
 source setup.sh
 #make
 #./bin/RunppTestAna -i /gpfs01/star/pwg/elayavalli/ppRun12Datapicos/ppJP2Run12/sum8.root -intype pico -c JetTree -trig ppJP2 -o Results/sum8_20pt30.root -N -1 -pj 20 30 -pc 0.2 30 -lja antikt -ec 1 -R 0.4 -hadcorr 0.9999999 -geantnum 0
-./bin/RunppTestAna -i /gpfs01/star/pwg/svomich/Jets_pp_2012_Raghav/ppRun12Datapicos/ppJP2Run12/$1 -intype pico -c JetTree -trig ppJP2 -o /gpfs01/star/pwg/svomich/Jets_pp_2012_Raghav/workDir/out/out_$1 -N -1 -pj 10 40 -pc 0.2 30 -lja antikt -ec 1 -R 0.4 -hadcorr 0.9999999 -geantnum 0
+#./bin/RunppTestAna -i /gpfs01/star/pwg/svomich/Jets_pp_2012_Raghav/ppRun12Datapicos/ppJP2Run12/$1 -intype pico -c JetTree -trig ppJP2 -o /gpfs01/star/pwg/svomich/Jets_pp_2012_Raghav/workDir/out/out_$1 -N -1 -pj 10 40 -pc 0.2 30 -lja antikt -ec 1 -R 0.4 -hadcorr 0.9999999 -geantnum 0
+./bin/RunppTestAna -i /gpfs01/star/pwg/svomich/Jets_pp_2012_Raghav/ppRun12Datapicos/ppJP2Run12/sum0.root -intype pico -c JetTree -trig ppJP2 -o /gpfs01/star/pwg/svomich/Jets_pp_2012_Raghav/workDir/out/out_$1 -N -1 -pj 10 40 -pc 0.2 30 -lja antikt -ec 1 -R 0.4 -hadcorr 0.9999999 -geantnum 0
